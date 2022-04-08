499005ed15172b51bca7d31eaad63acb34f79722
justindestry2 <84296902+justindestry2@users.noreply.github.com>
2022-04-08 11:24:24 -0700

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -5,25 +5,38 @@ print("Welcome to the Modulo-o-matic")
 
 import sys 
 
-firstnumber = int(input("What is the first number?"))
-secondnumber = int(input("What is the second number?"))
-moduloguess = int(input("What do you think 3 modulo 5 is?"))
+#all 4 int-variables copy-pasted 
+
+firstnumber = int(sys.argv[1])  
+#firstnumber = int(input("What is the first number?"))
+secondnumber = int(sys.argv[2])
+#secondnumber = int(input("What is the second number?"))
+moduloguess = int(sys.argv[3])
+#moduloguess = int(input("What do you think 3 modulo 5 is?"))
 moduloanswer = firstnumber % secondnumber
+#moduleguess2 = int(input("WHat do you think 5 modulo 3 is?"))
+moduleguess2 = int(sys.argv[4])
+moduloanswer2 = secondnumber % firstnumber
+
+
 #was thinking of how to put two arguments into a print statement 
 #thought about using two different variables
 """
 txt = "You answered {},"
 txt2 = " the correct answer is {}"
 """
-print("You answered {} the correct answer is {}".format(moduloguess, moduloanswer))
 
-moduleguess2 = int(input("WHat do you think 5 modulo 3 is?"))
-moduloanswer2 = secondnumber % firstnumber
-print("You answered {}, the correct answer is {}".format(moduleguess2,moduloanswer2))
-print("Keep up the good work learning!!")
 
 if len(sys.argv) > 0:
-    """
+    print("You answered {} the correct answer is {}".format(moduloguess, moduloanswer))
+    print("You answered {}, the correct answer is {}".format(moduleguess2,moduloanswer2))
+    print("Keep up the good work learning!!")
+
+
+""""
+if len(sys.argv) > 0:
+    print(sys.argv[1])
+    
     set firstnumber from system argument 1
     set secondnumber from system argument 2
     set moduloguess from system argument 3
