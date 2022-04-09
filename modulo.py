ca5cc139c0428a82a30eb54804fa52f33301185d
justindestry2 <84296902+justindestry2@users.noreply.github.com>
2022-04-08 21:48:10 -0700

    finished code?
    all seems to be good on mac

--- modulo.py
+++ modulo.py
@@ -7,15 +7,33 @@ import sys
 
 #all 4 int-variables copy-pasted 
 
-firstnumber = int(sys.argv[1])  
-#firstnumber = int(input("What is the first number?"))
-secondnumber = int(sys.argv[2])
-#secondnumber = int(input("What is the second number?"))
-moduloguess = int(sys.argv[3])
-#moduloguess = int(input("What do you think 3 modulo 5 is?"))
+#firstnumber = int(sys.argv[1])  
+firstnumber = (input("What is the first number?"))
+#secondnumber = int(sys.argv[2])
+secondnumber = (input("What is the second number?"))
+#couldnt format, had to concatenate str + int, switched int to str
+#print(type(firstnumber))
+#print(type(secondnumber))
+firstnumber = str(firstnumber)
+secondnumber = str(secondnumber)
+print(type(firstnumber))
+print(type(secondnumber))
+#moduloguess = int(sys.argv[3])
+#couldnt format these
+#moduloguess = int(input("What do you think {} modulo {} is?".format(firstnumber,secondnumber))
+moduloguess = (input("What do you think " + firstnumber + " modulo " + secondnumber + " is?"))
+#moduloanswer = firstnumber % secondnumber
+#couldnt format these
+moduloguess2 = (input("What do you think " + secondnumber + " modulo " + firstnumber + " is?"))
+#moduleguess2 = int(input("WHat do you think {} modulo {} is?".format(secondnumber,firstnumber))
+#moduleguess2 = int(sys.argv[4])
+
+#flip back to int before modulo calculations
+firstnumber = int(firstnumber)
+secondnumber = int(secondnumber)
+
+#modulo formulas
 moduloanswer = firstnumber % secondnumber
-#moduleguess2 = int(input("WHat do you think 5 modulo 3 is?"))
-moduleguess2 = int(sys.argv[4])
 moduloanswer2 = secondnumber % firstnumber
 
 
@@ -26,8 +44,14 @@ txt = "You answered {},"
 txt2 = " the correct answer is {}"
 """
 
+#with system arguments -testing in terminal
+#if len(sys.argv) > 0:
+    #print("You answered {} the correct answer is {}".format(moduloguess, moduloanswer))
+    #print("You answered {}, the correct answer is {}".format(moduleguess2,moduloanswer2))
+    #print("Keep up the good work learning!!")
 
-if len(sys.argv) > 0:
-    print("You answered {} the correct answer is {}".format(moduloguess, moduloanswer))
-    print("You answered {}, the correct answer is {}".format(moduleguess2,moduloanswer2))
-    print("Keep up the good work learning!!")
+
+#same print statements as above
+print("You answered {}, the correct answer is {}".format(moduloguess, moduloanswer))
+print("You answered {}, the correct answer is {}".format(moduloguess2,moduloanswer2))
+print("Keep up the good work learning!!")
--- money.py
+++ money.py
@@ -5,8 +5,9 @@ print("Welcome to the Money Translator!")
 
 import sys 
 
+
 #ask for average persons net worth (possibly argv it in)
-usernetworth = float(input("What is your income? "))
+usernetworth = float(input("What is your yearly income? "))
 #usernetworth = float(sys.argv[1])
 
 #name of person making the donation
@@ -14,22 +15,38 @@ donatorname = input("Who is making the AMAZING donation?")
 donatorname = donatorname.capitalize()
 
 #how much of a donation they are making
-donationamt = float(input("How much are they donating???"))
+donationamt = int(input("How much is {} donating???".format(donatorname)))
 #donationamt = float(sys.argv[2])
 
 #what their networth is
-donatornetworth = int(input("What is their networth in millions?"))
+donatornetworth = int(input("What is {}'s networth in millions?".format(donatorname)))
 #donatornetworth = int(sys.argv[3])
 
 actualnetworth = donatornetworth * 1000000 
+#reformating actualnetworth for print
+actualnetworth = "{0:,.2f}".format(actualnetworth)
 
 #print donators worth in actual amount
-print(donatorname, "'s networth is ", actualnetworth)
+print("If ", donatorname, "'s networth is $", actualnetworth)
+
+#reformating actualnetworth back to int after reformating to str
+actualnetworth = actualnetworth.replace(",","")
+actualnetworth = float(actualnetworth)
 
 #divide donation by networth
-donationdivide = donationamt // actualnetworth
-print(donationdivide)
+donationdivide = donationamt / actualnetworth
+#donationdivide = (donationdivide)
+#print(donationdivide)
 #print comparison
+userdonation = usernetworth * donationdivide
+userdonation = "{0:,.4f}".format(userdonation)
+#reformated usernetworth cause it was only showing 1 penny
+usernetworth = "{0:,.2f}".format(usernetworth)
+donationamt= "{0:,.2f}".format(donationamt)
+#changed one last time for formating in final string
+actualnetworth = "{0:,.2f}".format(actualnetworth)
 
-#compare that percentage to average persons networth
+print("Then one ${} donation for someone with their net worth of ${}, is the same as a ${} donation for someone with a net worth of ${} Generous?".format(donationamt, actualnetworth, userdonation, usernetworth))
+
+
 
